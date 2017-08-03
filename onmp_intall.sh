9a8da96b69428187d6300eaf0df145f66d4028ed
triton2 <842708096@qq.com>
2017-08-04 05:55:22 +0800

    0804-0555

--- onmp_intall.sh
+++ onmp_intall.sh
@@ -2,7 +2,7 @@
 ## @Author: triton
 # @Date:   2017-07-29 06:10:54
 # @Last Modified by:   triton2
-# @Last Modified time: 2017-08-03 23:28:13
+# @Last Modified time: 2017-08-04 05:54:54
 
 #软件包列表
 pkglist="unzip php7 php7-cgi php7-cli php7-fastcgi php7-fpm php7-mod-calendar php7-mod-ctype php7-mod-curl php7-mod-dom php7-mod-exif php7-mod-fileinfo php7-mod-ftp php7-mod-gd php7-mod-gettext php7-mod-gmp php7-mod-hash php7-mod-iconv php7-mod-intl php7-mod-json php7-mod-ldap php7-mod-session php7-mod-mbstring  php7-mod-mcrypt  php7-mod-mysqli php7-mod-opcache php7-mod-openssl php7-mod-pdo php7-mod-pcntl php7-mod-pdo-mysql php7-mod-phar php7-mod-session php7-mod-shmop php7-mod-simplexml php7-mod-soap php7-mod-sockets php7-mod-sqlite3 php7-mod-sysvmsg php7-mod-sysvsem php7-mod-sysvshm php7-mod-tokenizer php7-mod-xml php7-mod-xmlreader php7-mod-xmlwriter php7-mod-zip php7-pecl-dio php7-pecl-http php7-pecl-libevent php7-pecl-propro php7-pecl-raphf nginx zoneinfo-core zoneinfo-asia shadow-groupadd shadow-useradd libmariadb mariadb-server mariadb-client mariadb-client-extra"
@@ -329,14 +329,14 @@ onmp_restart()
     sleep 2
     nginx
     onmplist="nginx mysqld php-fpm"
-    num=0
+    num=1
     for i in $onmplist; do
         if [ `ps | grep $i |wc -l` -eq 1 ];then
             echo "$i 启动失败"
-            $num+1
+            let num++
         fi
     done
-    if [[ $num -gt 0 ]]; then
+    if [[ $num -gt 1 ]]; then
         echo "onmp启动失败"
         logger -t "【ONMP】" "启动失败"
     else
