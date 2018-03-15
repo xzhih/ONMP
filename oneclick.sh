b2011efef94c4d25b894234629628f0df2259995
triton2 <842708096@qq.com>
2018-03-15 22:10:11 +0800

    更新一键安装命令，更简洁，更方便

--- oneclick.sh
+++ oneclick.sh
@@ -5,10 +5,10 @@ rm -rf /opt/bin/onmp /opt/onmp
 mkdir -p /opt/onmp
 
 # 获取onmp脚本
-curl -lsSl https://raw.githubusercontent.com/xzhih/ONMP/master/onmp.sh > /opt/onmp/onmp.sh
+curl -kfsSL https://raw.githubusercontent.com/xzhih/ONMP/master/onmp.sh > /opt/onmp/onmp.sh
 chmod +x /opt/onmp/onmp.sh
 
 # 获取php探针文件
-curl -lsSl https://raw.githubusercontent.com/xzhih/ONMP/master/tz.php > /opt/onmp/tz.php
+curl -kfsSL https://raw.githubusercontent.com/xzhih/ONMP/master/tz.php > /opt/onmp/tz.php
 
 /opt/onmp/onmp.sh
